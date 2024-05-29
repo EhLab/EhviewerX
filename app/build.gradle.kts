4f5a9d2bffd6f6161b23ca646e123ab6c7b41874
KosWu <ws00298046@163.com>
2024-05-29 18:48:17 +0000

    add sentry

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,10 @@
 import com.mikepenz.aboutlibraries.plugin.DuplicateMode.MERGE
 import com.mikepenz.aboutlibraries.plugin.DuplicateRule.GROUP
+import io.sentry.android.gradle.extensions.InstrumentationFeature
 import java.time.Instant
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
+import java.util.EnumSet
 
 val isRelease: Boolean
     get() = gradle.startParameter.taskNames.any { it.contains("Release") }
@@ -16,6 +18,7 @@ plugins {
     id("com.mikepenz.aboutlibraries.plugin")
     id("org.mozilla.rust-android-gradle.rust-android")
     id("dev.shreyaspatil.compose-compiler-report-generator")
+    id("io.sentry.android.gradle") version "4.6.0"
 }
 
 android {
@@ -163,6 +166,20 @@ android {
     }
 
     namespace = "com.hippo.ehviewer"
+}
+
+sentry {
+    includeNativeSources.set(true)
+    includeSourceContext.set(true)
+    uploadNativeSymbols.set(true)
+    additionalSourceDirsForSourceContext.set(setOf("app/src/main/rust", "app/src/main/cpp"))
+
+    org.set(System.getenv("SENTRY_ORG"))
+    projectName.set(System.getenv("SENTRY_PROJECT_NAME"))
+    authToken.set(System.getenv("SENTRY_AUTH_TOKEN"))
+    tracingInstrumentation {
+        enabled.set(true)
+    }
 }
 
 dependencies {
